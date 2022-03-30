753b64dc8b607d9d09b8f9216aa0ef80ca48ea21
Zepeacedust <arnor0903@gmail.com>
2022-03-30 15:56:17 +0000

    progress on word_manager

--- loading_module.py
+++ loading_module.py
@@ -1,2 +1,48 @@
-class Word_generator:
-    pass
+import glob
+import random
+
+def load_file(filename):
+    with open(filename, "r") as file:
+        return file.read().split(",")
+def get_valid_files():
+    allowed = set()
+    for file in glob.glob("words/l*.csv"):
+        try:
+            allowed.add(int(file[7:][:-4]))
+        except Exception as e:
+            print(f'invalid filename: "{file}" in words folder')
+    return allowed
+
+def overwrite_file(filename,data):
+    with open(filename, "w") as file:
+        file.write(",".join(data))
+
+class Word_Manager:
+    def __init__(self):
+        self.allowed = get_valid_files()
+        self.loaded = dict()
+        self.updated = set()
+    def retrieve_word(self,length):
+        assert isinstance(length, int)
+        #if length has been loaded before
+        if length in self.loaded:
+            #return random word with given length
+            return random.choice(self.loaded[length])
+        #else load it
+        self.load(length)
+        #then return random word with given length
+        return random.choice(self.loaded[length])
+    def load(self,length):
+        if not length in self.allowed:
+            print("file does not exist, please try another")
+        print(f"Loading words of length {length}.")
+        self.loaded[length] = load_file(f"words/l{length}.csv")
+        
+    def update(self, word):
+        length = len(word)
+        self.updated.add(length)
+        self.loaded[length].append(word.lower())
+    
+    def exit(self):
+        for length in self.updated:
+            overwrite_file(f"words/l{length}.csv", self.loaded[length])
--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import random
 from game import Game
-from loading_module import Word_generator
-word_database_filename = "words.csv"
+from loading_module import Word_Manager
+word_database_path = "words"
 
 def load_words(filename) -> list:
     pass
 
-WORDS = load_words(word_database_filename)
+WORDS = Word_Manager()
+
