b107b01323fe464c4751a16133fd39e23a73f9d3
Huginn-Muninn <tomas.orri.arnarsson@gmail.com>
2022-03-30 15:48:50 +0000

    guess_fist_round

--- game.py
+++ game.py
@@ -4,7 +4,33 @@ class Game:
         self.lenght  = length
         self.guesses = guesses
         self.selected = word
+        self.attempts = 0
+        self.gameStat = ""
     def save(self):
         pass
     def guess(self,attempt):
-        pass
+        if len(attempt) != self.lenght:
+            print("incorrect length")
+            return
+        fin = ""
+        count = 0
+        for x in attempt.lower():
+            if x == self.selected[count].lower():
+                fin += "C"
+            elif x in self.selected.lower():
+                fin += "c"
+            else:
+                fin += "-"
+            count += 1
+        for x in fin:
+            if x != "C":
+                print(fin)
+                self.attempts += 1
+                return
+        print(fin)
+        print("Congrats you won!")
+
+
+g = Game(5,5,"Lorem")
+
+g.guess("lorem")
