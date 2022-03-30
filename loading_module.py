2612761c3222d3fc44d3b4c46f6013730c959f93
Zepeacedust <arnor0903@gmail.com>
2022-03-30 16:09:52 +0000

    fix update for length that are not loaded

--- loading_module.py
+++ loading_module.py
@@ -5,6 +5,7 @@ def load_file(filename):
     with open(filename, "r") as file:
         return file.read().split(",")
 def get_valid_files():
+    #finna öll leyfð files í words
     allowed = set()
     for file in glob.glob("words/l*.csv"):
         try:
@@ -28,20 +29,24 @@ class Word_Manager:
         if length in self.loaded:
             #return random word with given length
             return random.choice(self.loaded[length])
-        #else load it
-        self.load(length)
-        #then return random word with given length
-        return random.choice(self.loaded[length])
+        #else try to load it
+        if self.load(length):
+            return random.choice(self.loaded[length])
+            #then return random word with given length
     def load(self,length):
         if not length in self.allowed:
-            print("file does not exist, please try another")
+            print("No words of that length exist, please try another")
+            return
         print(f"Loading words of length {length}.")
         self.loaded[length] = load_file(f"words/l{length}.csv")
         
     def update(self, word):
         length = len(word)
         self.updated.add(length)
-        self.loaded[length].append(word.lower())
+        if length in self.loaded:
+            self.loaded[length].append(word.lower())
+        else:
+            self.loaded[length] = [word.lower()]
     
     def exit(self):
         for length in self.updated:
