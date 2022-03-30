9b5aa32413fc69a0900a6ad8b979326deeb6b685
Zepeacedust <arnor0903@gmail.com>
2022-03-30 17:04:38 +0000

    fix error while loading

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 import random
 from game import Game
 from loading_module import Word_Manager
-word_database_path = "words"
-
-def load_words(filename) -> list:
-    pass
 
 WORDS = Word_Manager()
 
+while True:
+    guesses = int(input("How many guesses do you want? "))
+    letters = int(input("How many letters do you want? "))
+    word = WORDS.retrieve_word(letters)
+    while word == None:
+        print(f"No words of length {letters}")
+        word = WORDS.retrieve_word(letters)
+    game = Game(guesses,letters,word)
+    
+        
